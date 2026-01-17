1451435d2748eb9febf519c8f533100f353c342c
matthewpohl <matthewpohl@uvic.ca>
2026-01-17 15:09:04 -0800

    assn1_part_1.py

--- assn1 part 1.py
+++ assn1 part 1.py
@@ -8,37 +8,41 @@ lcd = i2c_lcd.I2cLcd(screen, 0x27, 2, 16)
 press_start = None
 
 while True:
-    if button.value() == 0 and press_start is None: #button pressed and prevent bounce/recording several start times
-        press_start = time.ticks_ms()
+    if button.value() == 0 and press_start is None: # button pressed and prevent bounce/recording several start times
+        press_start = time.ticks_ms() # assign start_time as the time when the button is initially pressed
         
-    elif button.value() == 1 and press_start is not None:  # press_start is not blank
-        press_end = time.ticks_ms()
-        duration = time.ticks_diff(press_end, press_start) #time that button was released - time it was started in ms
+    elif button.value() == 1 and press_start is not None: # press_start is not blank
+        press_end = time.ticks_ms() # assign press_end as the time when the button is released
+        duration = time.ticks_diff(press_end, press_start) # time that button was released - time it was started in ms
         press_start = None # clears start time so button can be pressed again
         
+        # if the button is pressed for a short time (0 ms < t < 500 ms)
         if duration < 500:
             lcd.clear()
-            lcd.move_to(2, 0) #center text
+            lcd.move_to(2, 0) # center text
             lcd.putstr('short press')
             lcd.move_to(5, 1)
-            lcd.putstr(str(duration)) #print a variable (i think puts it into a string instead of integer?)
+            lcd.putstr(str(duration)) # print a variable as a string
             lcd.move_to(9,1)
-            lcd.putstr('ms') #display units
+            lcd.putstr('ms') # display units
             time.sleep(0.3)
-        
+            
+        # if the button is pressed for a long time (500 ms < t < 2000 ms)
         elif duration >= 500 and duration <= 2000:
             lcd.clear()
-            lcd.move_to(3, 0) #center the text
+            lcd.move_to(3, 0) # center the text
             lcd.putstr('long press')
             lcd.move_to(5, 1)
-            lcd.putstr(str(duration)) #print a variable
+            lcd.putstr(str(duration)) # print a variable as a string
             lcd.move_to(9,1)
-            lcd.putstr('ms') #display units
+            lcd.putstr('ms') # display units
             time.sleep(0.3)
-        
+            
+        # if the button is pressed for too long (t > 2000 ms)
+        # *self-destruct sequence
         elif duration > 2000:
             lcd.clear()
-            lcd.move_to(1, 0) #center the text
+            lcd.move_to(1, 0) # center the text
             lcd.putstr('press too long')
             lcd.move_to(1, 1)
             lcd.putstr('self destruct')
@@ -58,6 +62,6 @@ while True:
                 lcd.putstr('boom!')
                 time.sleep(0.3)
 
-    time.sleep_ms(10) #debounce
+    time.sleep_ms(10) # let the microprocessor rest momentarily
 
     
