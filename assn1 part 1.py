3e2e21a59808a52f7f338f69d001fccbd484245d
matthewpohl <matthewpohl@uvic.ca>
2026-01-18 14:51:20 -0800

    assn1 part 1.py
    update comments

--- assn1 part 1.py
+++ assn1 part 1.py
@@ -1,6 +1,8 @@
+# setup
 from machine import Pin, I2C
 import i2c_lcd, time
 
+# configure
 button = Pin(12, Pin.IN, Pin.PULL_UP)
 screen = I2C(0, scl=Pin(22), sda=Pin(21))
 lcd = i2c_lcd.I2cLcd(screen, 0x27, 2, 16)
@@ -20,22 +22,22 @@ while True:
         if duration < 500:
             lcd.clear()
             lcd.move_to(2, 0) # center text
-            lcd.putstr('short press')
-            lcd.move_to(5, 1)
-            lcd.putstr(str(duration)) # print a variable as a string
+            lcd.putstr('short press') # display 'short press'
+            lcd.move_to(5, 1) # center text
+            lcd.putstr(str(duration)) # print the duration of the press
             lcd.move_to(9,1)
-            lcd.putstr('ms') # display units
+            lcd.putstr('ms') # display units of time
             time.sleep(0.3)
             
         # if the button is pressed for a long time (500 ms < t < 2000 ms)
         elif duration >= 500 and duration <= 2000:
             lcd.clear()
-            lcd.move_to(3, 0) # center the text
-            lcd.putstr('long press')
+            lcd.move_to(3, 0) # center text
+            lcd.putstr('long press') # display 'long press'
             lcd.move_to(5, 1)
-            lcd.putstr(str(duration)) # print a variable as a string
+            lcd.putstr(str(duration)) # print the duration of the press
             lcd.move_to(9,1)
-            lcd.putstr('ms') # display units
+            lcd.putstr('ms') # display units of time
             time.sleep(0.3)
             
         # if the button is pressed for too long (t > 2000 ms)
@@ -43,19 +45,21 @@ while True:
         elif duration > 2000:
             lcd.clear()
             lcd.move_to(1, 0) # center the text
-            lcd.putstr('press too long')
-            lcd.move_to(1, 1)
-            lcd.putstr('self destruct')
+            lcd.putstr('press too long') # display 'press too long'
+            lcd.move_to(1, 1) # move to second line, and center text
+            lcd.putstr('self destruct') # display 'self destruct'
             time.sleep(2)
             lcd.clear()
             count = 3
-            
-            while count >= 0: #count down
+
+            # count down to 0 from 3
+            while count >= 0:
                 lcd.move_to(7, 1) 
                 lcd.putstr(str(count))
                 count -= 1
                 time.sleep(1)
-                
+
+            # after 0, display 'boom!' on the LCD
             else:
                 lcd.clear()
                 lcd.move_to(6, 0)
@@ -63,5 +67,3 @@ while True:
                 time.sleep(0.3)
 
     time.sleep_ms(10) # let the microprocessor rest momentarily
-
-    
