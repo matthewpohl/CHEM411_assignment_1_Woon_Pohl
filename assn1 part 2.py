840ecc5623b5a10aec3f98e4e03af8664a9f453f
matthewpohl <matthewpohl@uvic.ca>
2026-01-18 14:47:51 -0800

    assn1 part 2.py
    updated comments and removed line 42

--- assn1 part 2.py
+++ assn1 part 2.py
@@ -1,13 +1,15 @@
+# setup
 from machine import Pin, I2C, ADC
 import time, i2c_lcd
 
+# configure
 button_left = Pin(14, Pin.IN, Pin.PULL_UP)
 button_right = Pin(12, Pin.IN, Pin.PULL_UP)
 lcd_device = I2C(0, scl=Pin(22), sda=Pin(21))
 lcd = i2c_lcd.I2cLcd(lcd_device, 0x27, 2, 16)
 
 
-passcode = 'LLRR' # define passcode as a list 
+passcode = 'LLRR' # define passcode as a list # define passcode solution
 
 current_input = ''
 last_input_time = time.ticks_ms()
@@ -22,7 +24,6 @@ while True:
         if button_left.value() == 0:
             current_input += 'L' # append 'L' to the list
             last_input_time == time.ticks_ms()
-            print('left')
             lcd.clear()
             lcd.move_to(0,0)
             lcd.putstr(str(current_input)) # print the list of inputs e.g., LLRL
@@ -32,33 +33,35 @@ while True:
         if button_right.value() == 0:
             current_input += 'R' # append 'R' to the list
             last_input_time == time.ticks_ms()
-            print('right')
             lcd.clear()
             lcd.move_to(0,0)
             lcd.putstr(str(current_input)) # print the list of inputs e.g., LRR
             time.sleep_ms(200)
 
+        # if the passcode is 4 characters long, check if it matches the 'passcode' and display whether or not it is correct on the LCD
         if len(current_input) == len(passcode):
-            if time.ticks_diff(time.ticks_ms(), last_input_time) > 2000 or len(current_input) >= len(passcode):
-                if current_input == passcode:
-                    lcd.clear()
-                    lcd.move_to(0,0)
-                    lcd.putstr('access granted')
-                    lcd.move_to(0,1)
-                    lcd.putstr('correct passcode')
-                    time.sleep_ms(100)
-
-                else:
-                    lcd.clear()
-                    lcd.move_to(0,0)
-                    lcd.putstr('access denied')
-                    lcd.move_to(0,1)
-                    lcd.putstr('wrong passcode')
-                    time.sleep_ms(100)
-                current_input = ''
-                time.sleep_ms(2000)
-                lcd.move_to(0,0)
+            if current_input == passcode:
                 lcd.clear()
-                lcd.putstr('ready 4 input')
+                lcd.move_to(0,0)
+                lcd.putstr('access granted') # display 'access granted' on the LCD
+                lcd.move_to(0,1) # move to the next line
+                lcd.putstr('correct passcode') # display 'correct passcode' on the LCD
+                time.sleep_ms(100)
+
+            else:
+                lcd.clear()
+                lcd.move_to(0,0)
+                lcd.putstr('access denied') # display 'access denied' on the LCD
+                lcd.move_to(0,1) # move to next line
+                lcd.putstr('wrong passcode') # display 'wrong passcode' on the LCD
+                time.sleep_ms(100)
+
+            # reset for next input
+            current_input = ''
+            time.sleep_ms(2000)
+            lcd.move_to(0,0)
+            lcd.clear()
+            lcd.putstr('ready 4 input')
                 
+
 
